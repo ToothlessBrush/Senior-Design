4fac99e0871e061d707a81dd05ec3af606cba4c9
John Klein <johnklein514@gmail.com>
2025-09-10 21:53:47 -0500

    Remove build artifacts and cache files from tracking

--- FlightController/build/CMakeFiles/flash_util.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/flash_util"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/flash_util.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
